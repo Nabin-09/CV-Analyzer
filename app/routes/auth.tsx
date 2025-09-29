1da0b4dabfd06f6e2a2ae844548117f0ae5fe222
Nabin <sharmanabin.0910@gmail.com>
2025-09-29 21:43:24 +0530

    Loader and Buffer buttons

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,12 +1,44 @@
 import React from "react";
+import { usePuterStore } from "~/lib/puter";
 
-const 
+export const meta = ()=>([
+    {title : 'Rezlyzer | Auth'},
+    {name : 'description' , content : 'Log into your account'}
+])
 
 const Auth =  ()=>{
+    const {isLoading ,auth} = usePuterStore();
     return(
-        <div>
-            Auth
-        </div>
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center" >
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome!</h1>
+                        <h2>LogIn to continue your journey</h2>
+                    </div>
+                    <div>
+                        {isLoading?(
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
+                        ) : (
+                            <>
+                            {auth.isAuthenticated?(
+                                <button className="auth-button onClick = {auth.signOut}">
+                                    <p>Log Out</p>
+                                </button>
+                            ) : (
+                                 <button className="auth-button onClick = {auth.signIn}">
+                                    <p>Log In</p>
+                                </button>
+                            )}
+                            </>
+                        )}
+                    </div>
+                </section>
+
+            </div>
+        </main>
     )
 }
 
